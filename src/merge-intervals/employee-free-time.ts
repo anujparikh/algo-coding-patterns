980ab95957795e183f8580e85876114a0abb968f
Anuj Parikh <anujparikh1@gmail.com>
2022-07-10 23:47:57 -0500

    WIP: Employee Free time with min heap

--- src/merge-intervals/employee-free-time.ts
+++ src/merge-intervals/employee-free-time.ts
@@ -1,4 +1,5 @@
 import { Interval } from "../utils/Interval";
+import Heap from 'collections/heap'; //http://www.collectionsjs.com
 
 const commonFreeTime = (schedules: Array<Array<Interval>>): Array<Interval> => {
     const commonIntervals: Array<Interval> = [];
@@ -12,6 +13,33 @@ const commonFreeTime = (schedules: Array<Array<Interval>>): Array<Interval> => {
             commonIntervals.push(new Interval(previous.end, current.start));
         }
         counter++;
+    }
+    return commonIntervals;
+}
+
+type EmployeeSchedule = {
+    interval: Interval,
+    employeeNumber: number,
+    currentInterval: number
+}
+
+const commonFreeTimeUsingMinHeap = (schedules: Array<Array<Interval>>): Array<Interval> => {
+    const commonIntervals: Array<Interval> = [];
+    const minHeap = new Heap([], null, (a: EmployeeSchedule, b: EmployeeSchedule) => b.interval.start - a.interval.start)
+    for (let i = 0; i < schedules.length; i++) {
+        minHeap.push({ interval: schedules[i][0], employeeNumber: i, currentInterval: 0 })
+    }
+    while (minHeap.length !== 0) {
+        let previous = minHeap.pop();
+        let current = minHeap.peek() && minHeap.pop();
+        if (previous.interval.end >= current.interval.start) {
+            previous = current;
+            minHeap.push(current);
+        } else {
+            commonIntervals.push(new Interval(previous.interval.end, current.interval.start));
+            previous.currentInterval + 1 < schedules[previous.employeeNumber].length && minHeap.push({ interval: schedules[previous.employeeNumber][previous.currentInterval + 1], employeeNumber: previous.employeeNumber, currentInterval: previous.currentInterval + 1 });
+            current.currentInterval + 1 < schedules[current.employeeNumber].length && minHeap.push({ interval: schedules[current.employeeNumber][current.currentInterval + 1], employeeNumber: current.employeeNumber, currentInterval: current.currentInterval + 1 });
+        }
     }
     return commonIntervals;
 }
