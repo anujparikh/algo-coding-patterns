ac2d9c990739d8db7e1778a40738a86cddb92165
Anuj Parikh <anujparikh1@gmail.com>
2022-07-14 21:25:57 -0500

    Valid anagram

--- src/array-and-hashing/README.md
+++ src/array-and-hashing/README.md
@@ -3,6 +3,7 @@
 ## Blind75 questions
 
 - Contains Duplicate: [Solution](/src/array-and-hashing/contains-duplicate.ts) => [Problem Link](https://leetcode.com/problems/contains-duplicate/)
+- Valid Anagram: [Solution](/src/array-and-hashing/valid-anagram.ts) => [Problem Link](https://leetcode.com/problems/valid-anagram/)
 
 
 ## Useful Snipets and tricks
---
+++ src/array-and-hashing/valid-anagram.ts
@@ -0,0 +1,12 @@
+function isAnagram(s1: string, s2: string): boolean {
+    if (s1.length !== s2.length) return false;
+    const charMap: { [key: string]: number } = {};
+    for (let i = 0; i < s1.length; i++) {
+        charMap[s1[i]] = (charMap[s1[i]] || 0) + 1;
+        charMap[s2[i]] = (charMap[s2[i]] || 0) - 1;
+
+        if (charMap[s1[i]] === 0) delete charMap[s1[i]];
+        if (charMap[s2[i]] === 0) delete charMap[s2[i]];
+    }
+    return Object.keys(charMap).length === 0;
+};
