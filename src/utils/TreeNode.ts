9f54e7753ce19f8e6c7ca7ce230e06da563cfc65
Anuj Parikh <anujparikh1@gmail.com>
2022-08-02 22:59:04 -0500

    utils class for TreeNode with next pointer

--- src/utils/TreeNode.ts
+++ src/utils/TreeNode.ts
@@ -9,3 +9,16 @@ export class TreeNode {
         this.right = right;
     }
 }
+
+export class TreeNodeWithNext {
+    val: number
+    left: TreeNodeWithNext | null
+    right: TreeNodeWithNext | null
+    next: TreeNodeWithNext | null
+    constructor(val?: number, left?: TreeNodeWithNext, right?: TreeNodeWithNext, next?: TreeNodeWithNext) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+        this.next = (next === undefined ? null : next)
+    }
+}
