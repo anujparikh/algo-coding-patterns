a4d674981ce96f963a50149362769986181f75e6
Anuj Parikh <anujparikh1@gmail.com>
2022-08-02 22:38:08 -0500

    Populating Next Right Pointers in Each Node

--- src/tree-breath-first-search/README.md
+++ src/tree-breath-first-search/README.md
@@ -10,3 +10,4 @@
 - Average of Levels in Binary Tree: [Solution](/src/tree-breath-first-search/level-averages.ts) => [Problem Link](https://leetcode.com/problems/average-of-levels-in-binary-tree/)
 - Minimum Depth of Binary Tree: [Solution](/src/tree-breath-first-search/minimum-depth-binary-tree.ts) => [Problem Link](https://leetcode.com/problems/minimum-depth-of-binary-tree/)
 - Level order successor: [Solution](/src/tree-breath-first-search/level-order-successor.ts) => [Problem Link](https://www.geeksforgeeks.org/level-order-successor-of-a-node-in-binary-tree/)
+- Populating Next Right Pointers in Each Node: [Solution](/src/tree-breath-first-search/populate-next-pointers.ts) => [Problem Link](https://leetcode.com/problems/populating-next-right-pointers-in-each-node/)
---
+++ src/tree-breath-first-search/populate-next-pointers.ts
@@ -0,0 +1,32 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    next: TreeNode | null
+    constructor(val?: number, left?: TreeNode, right?: TreeNode, next?: TreeNode) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+const populateNextPointer = (root: TreeNode | null): TreeNode | null => {
+    if (root === null) return root;
+    let queue: Array<TreeNode> = [];
+    queue.push(root);
+    while (queue.length !== 0) {
+        let size = queue.length;
+        let previous = queue.shift();
+        for (let i = 0; i < size; i++) {
+            previous?.left && queue.push(previous.left);
+            previous?.right && queue.push(previous.right);
+            const next = i === size - 1 ? undefined : queue.shift();
+            if (previous) {
+                previous.next = next || null;
+            }
+            previous = next;
+        }
+    }
+    return root;
+}
